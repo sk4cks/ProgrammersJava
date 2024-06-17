1d51b488cee9fc1d2f3736388576444fd24c3596
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-06-17 21:28:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,37 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        List<String> tmpCards1 = new ArrayList<>();
-        List<String> tmpCards2 = new ArrayList<>();
-
-        Loop1 :
+        int cards1Index = 0;
+        int cards2Index = 0;
+        
         for(int i=0; i<goal.length; i++){
-            for(int j=0; j<cards1.length; j++){
-                if(goal[i].equals(cards1[j])){
-                    tmpCards1.add(goal[i]);
-                    continue Loop1;
-                }
-            }
-
-            for(int j=0; j<cards2.length; j++){
-                if(goal[i].equals(cards2[j])){
-                    tmpCards2.add(goal[i]);
-                    continue Loop1;
-                }
+            if(cards1Index < cards1.length && goal[i].equals(cards1[cards1Index])){
+                cards1Index++;
+            }else if(cards2Index < cards2.length && goal[i].equals(cards2[cards2Index])){
+                cards2Index++;
+            }else{
+                return "No";
             }
         }
-
-        for(int i=0; i<tmpCards1.size(); i++){
-            if(!cards1[i].equals(tmpCards1.get(i))) return "No";
-        }
-
-        for(int i=0; i<tmpCards2.size(); i++){
-            if(!cards2[i].equals(tmpCards2.get(i))) return "No";
-        }
-
+    
         return answer;
     }
 }
