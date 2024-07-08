0bfa146f7292e64638a63ce2670f9df6b5e398a7
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-07-08 15:47:01 +0900

    [level 2] Title: 귤 고르기, Time: 62.39 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,16 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer,Integer> map = new HashMap<>();
-        List<Integer> countList = new ArrayList<>();
         int count = 0;
         int answer = 0;
         
         for(int i=0; i< tangerine.length; i++){
             map.put(tangerine[i],map.get(tangerine[i]) == null? 1 : map.get(tangerine[i])+1);
         }
-        for(Integer key : map.keySet()){
-            countList.add(map.get(key));
-        }
+        
+        List<Integer> countList = new ArrayList<>(map.values());
         Collections.sort(countList,Collections.reverseOrder());
 
         for(int i=0; i<countList.size(); i++){
