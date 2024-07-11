e6f0a1134e948b2a586c3c56faf98576ccef5b3b
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-07-11 17:14:00 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> copyMap = new HashMap<>();
+
+        for(int i=0; i<want.length; i++){
+            map.put(want[i],number[i]);
+        }
+
+        for(int i=0; i< discount.length-9; i++){
+            boolean flag = false;
+            copyMap = new HashMap<>(map);
+            for(int j=0; j<10; j++){
+                if(copyMap.getOrDefault(discount[i+j],0) <= 0) break;
+                copyMap.put(discount[i+j], copyMap.get(discount[i+j]) - 1 );
+
+                if(j==9) flag = true;
+            }
+            if(flag) answer++;
+        }
+        return answer;
+    }
+}
