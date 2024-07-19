bd2728a199a8a0e3336a76004c6df3bdcc920da5
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-07-19 17:10:17 +0900

    [level 2] Title: 타겟 넘버, Time: 8.37 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,19 @@
 class Solution {
-    static int answer = 0;
+    int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        search(numbers,target,-numbers[0],0);
-        search(numbers,target,+numbers[0],0);
+        dfs(numbers,target,0,0);
         
         return answer;
     }
     
-    static void search(int[] numbers,int target,int number,int index){
-        if(index==numbers.length-1) {
+    void dfs(int[] numbers,int target,int number,int index){
+        if(index==numbers.length) {
             if(number == target) answer++;
             return;
         }
-        search(numbers,target,number+numbers[index+1],index+1);
-        search(numbers,target,number-numbers[index+1],index+1);
+        dfs(numbers,target,number+numbers[index],index+1);
+        dfs(numbers,target,number-numbers[index],index+1);
     }
     
 }
