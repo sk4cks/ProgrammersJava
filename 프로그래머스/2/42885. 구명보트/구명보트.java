3ecb8579a0b26cbf0e8917120c49d9c56f6be7a8
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-07-26 12:16:00 +0900

    [level 2] Title: 구명보트, Time: 10.53 ms, Memory: 56 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int index = 0;
+        int answer = 0;
+        Arrays.sort(people);
+
+        for(int i=people.length-1; i>=0; i--){
+            if(index > i) break;
+            
+            if(people[i]+people[index] <= limit){
+                index++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
