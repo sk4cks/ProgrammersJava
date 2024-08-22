c3c75fb00ade5a2f76d54113fc010fa38125f90d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-08-22 18:52:15 +0900

    [level 2] Title: 짝지어 제거하기, Time: 144.26 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            stack.push(s.charAt(i));
+            while (stack.size()>=2
+                    && stack.get(stack.size()-1)==stack.get(stack.size()-2)){
+                stack.pop();
+                stack.pop();
+            }
+        }
+        if(stack.size()==0) answer = 1;
+
+        return answer;
+    }
+}
