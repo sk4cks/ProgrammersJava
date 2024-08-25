096b1ff61fe9502a9ea2ee795383bc10796a0eea
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-08-25 23:30:22 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.32 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,19 +2,15 @@ class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
         boolean flag = true;
+
         for(int i=0; i<s.length(); i++){
-            String word =  String.valueOf(s.charAt(i));
-            if(word.equals(" ")) {
-                sb.append(" ");
-                flag = true;
-            }else {
-                if(flag){
-                    sb.append(word.toUpperCase());
-                    flag = false;
-                }else{
-                    sb.append(word.toLowerCase());
-                }
+            String word =  String.valueOf(s.charAt(i)).toLowerCase();
+            if(word.equals(" ")) flag = true;
+            else if(flag) {
+                word = word.toUpperCase();
+                flag = false;
             }
+            sb.append(word);
         }
         
         return sb.toString();
