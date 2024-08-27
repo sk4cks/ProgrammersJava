62a135da1c63d55cca0009ce177a865b3c4559f2
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-08-27 19:48:18 +0900

    [level 2] Title: 피보나치 수, Time: 22.97 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int n) {
+        Queue<Integer> que = new LinkedList<>();
+        int answer = 0;
+        int index = 0;
+        que.add(0);
+        que.add(1);
+
+        while (index < n){
+            index++;
+            que.add((que.poll() + que.peek())%1234567);
+        }
+        
+        answer = que.peek();
+        
+        return answer;
+    }
+}
