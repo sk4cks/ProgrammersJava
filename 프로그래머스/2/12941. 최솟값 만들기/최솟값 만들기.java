bd86a78393836ffa643a2d3d3ec914655197302d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-08-28 20:15:16 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.90 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,13 +6,12 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        Integer[] arr = Arrays.stream(B).boxed().toArray(Integer[]::new);
 
         Arrays.sort(A);
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(B);
 
         for(int i=0; i<A.length; i++){
-            answer += A[i]*arr[i];
+            answer += A[i]*B[B.length-1-i];
         }
 
         return answer;
