d330aadd8258b11af52b13ce4d85bfa333db386a
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-08-29 19:54:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.59 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        int[] nums = Arrays.asList(arr).stream()
+            .mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
+        answer = nums[0]+" "+nums[nums.length-1];
+        return answer;
+    }
+}
