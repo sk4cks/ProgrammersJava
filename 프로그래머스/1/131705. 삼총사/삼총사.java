05ced1446e0da2cfa8b61c8aecbd262a5b7bcdcf
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-09-09 21:13:35 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=0; i<number.length-2; i++){
             for(int j=i+1; j<number.length-1; j++){
                 for(int k=j+1; k< number.length; k++){
-                    if(number[i]+number[j]+number[k]==0) answer++;
+                    if(number[i]+number[j]+number[k]==0) 
+                        answer++;
                 }
             }
         }
