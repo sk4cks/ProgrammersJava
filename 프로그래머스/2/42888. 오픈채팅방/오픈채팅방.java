b2c9ea27f5c8521062f6e6e8eeec1a95127b9d23
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-09-12 20:27:09 +0900

    [level 2] Title: 오픈채팅방, Time: 458.81 ms, Memory: 265 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -11,16 +11,17 @@ class Solution {
 
         for(int i=0; i<record.length; i++) {
             String[] arr = record[i].split(" ");
-            if(map.get(arr[0]) != null){
-                list.add(map.get(arr[0])+","+arr[1]);
-            }
             if(arr.length>2) nameMap.put(arr[1],arr[2]);
         }
 
-        for(int i=0; i<list.size(); i++){
-            String[] arr = list.get(i).split(",");
-            list.set(i,String.format(arr[0],nameMap.get(arr[1])));
+        for(int i=0; i<record.length; i++){
+            String[] arr = record[i].split(" ");
+
+            if(map.get(arr[0]) != null){
+                list.add(String.format(map.get(arr[0]),nameMap.get(arr[1])));
+            }
         }
+        
         return list.toArray(new String[0]);
     }
 }
