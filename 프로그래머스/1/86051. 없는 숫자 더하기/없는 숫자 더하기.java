97719e51cc6eaf6fbeaf270ab905b3db3c82af30
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-09-14 21:08:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,19 +1,9 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        List<Integer> list = 
-            Arrays.stream(numbers).boxed().collect(Collectors.toList());
-        int[] arr = new int[10];
+        int answer = 45;
 
-        for(int i=1; i<arr.length; i++){
-            arr[i] = i;
-        }
-
-        for(int i=0; i<arr.length; i++){
-           if(list.indexOf(arr[i]) < 0) answer += arr[i] ;
+        for(int i=0; i<numbers.length; i++){
+            answer -= numbers[i];
         }
         
         return answer;
