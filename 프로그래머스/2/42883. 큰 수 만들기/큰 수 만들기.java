132720b10d346a8f4b3311c8a21a57a005fb1e88
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-11-16 21:17:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 6683.34 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,14 +1,15 @@
 class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
+        char[] arr = number.toCharArray();
         int index = 0;
         char max = 0;
 
-        for(int i=0; i<number.length()-k; i++) {
+        for(int i=0; i<arr.length-k; i++) {
             max = 0;
             for(int j=index; j<=i+k; j++) {
-                if(number.charAt(j) > max) {
-                    max = number.charAt(j);
+                if(arr[j] > max) {
+                    max = arr[j];
                     index = j+1;
                 }
             }
