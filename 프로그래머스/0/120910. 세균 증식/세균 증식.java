44ec34f5a96e2b9e484ab4aa17c2bdc2409e7b38
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-12-03 22:21:10 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n, int t) {
-        
-        for(int i=0; i<t; i++) {
-            n*=2;
-        }
-        
-        return n;
+        return n * (int) Math.pow(2,t);
     }
 }
