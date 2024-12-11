343e1738eedb78ab06649cc58addd57e3aeb0ede
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-12-11 21:40:04 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 21.37 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -5,14 +5,9 @@ class Solution {
         int answer = 0;
         int aGcd = getGcd(arrayA);
         int bGcd = getGcd(arrayB);
-        List<Integer> aCdList = getCdList(aGcd);
-        List<Integer> bCdList = getCdList(bGcd);
 
-        aCdList.sort(Comparator.reverseOrder());
-        bCdList.sort(Comparator.reverseOrder());
-
-        answer = setAnswer(aCdList,arrayB,answer);
-        answer = setAnswer(bCdList,arrayA,answer);
+        answer = setAnswer(aGcd,arrayB,answer);
+        answer = setAnswer(bGcd,arrayA,answer);
         
         return answer;
     }
@@ -30,32 +25,11 @@ class Solution {
         return getGcd(b, a % b);
     }
     
-    List<Integer> getCdList(int gcd) {
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=1; i<=Math.sqrt(gcd); i++) {
-            if(gcd%i == 0) {
-                if(i != gcd/i) list.add(gcd/i);
-                list.add(i);
-            }
+    int setAnswer(int gcd, int[] arr, int answer) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]%gcd==0) return answer;
         }
 
-        return list;
-    }
-    
-    int setAnswer(List<Integer> list, int[] arr, int answer) {
-        Loop:
-        for(int i=0; i<list.size()-1; i++) {
-            int cd = list.get(i);
-            for(int j=0; j<arr.length; j++) {
-                if(arr[j]%cd==0) break;
-                if(j==arr.length-1) {
-                    answer = Math.max(answer,cd);
-                    break Loop;
-                }
-            }
-        }
-
-        return answer;
+        return Math.max(answer,gcd);
     }
 }
