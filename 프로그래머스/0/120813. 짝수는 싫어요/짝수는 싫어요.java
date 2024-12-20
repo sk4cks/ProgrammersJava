b0d0bb71623d9d32fad3a86e710905ca7e56aec4
나카롱 <109573477+sk4cks@users.noreply.github.com>
2024-12-20 18:27:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.88 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++) {
+            if(i%2==1) list.add(i);
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
