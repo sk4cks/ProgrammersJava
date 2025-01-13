712031b4749e7f7163d0a82cc8fc9849c5ee363d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-01-13 21:24:05 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.88 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list)
+            .filter(item -> !item.contains(ex))
+            .collect(Collectors.joining(""));
+    }
+}
