a2dc03890ff38de456e9d01c5ca66e389acb3929
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-03-19 22:12:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.41 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        char[] arr = my_string.toCharArray();
+        Arrays.sort(arr);
+        
+        return String.valueOf(arr);
+    }
+}
