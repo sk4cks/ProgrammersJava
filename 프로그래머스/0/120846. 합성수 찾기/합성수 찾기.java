62a28b29af7ba6a9c899d2679309b210169f24d8
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-04-07 21:22:45 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=4; i<=n; i++) {
+            int count = 0; 
+            for(int j=1; j<=Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    count++;
+                    if(j != i/j) count++;
+                }
+                
+                if(count >= 3) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
