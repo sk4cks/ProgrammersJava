e2842b400c307d22699f24efca02d73d16ef123d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-04-21 21:40:00 +0900

    [level 0] Title: A로 B 만들기, Time: 0.96 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
+        
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+        
+        return Arrays.equals(beforeArr, afterArr) ? 1 : 0;
+    }
+}
