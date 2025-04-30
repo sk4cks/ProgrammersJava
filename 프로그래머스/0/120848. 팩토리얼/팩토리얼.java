e145d02b66643b86042900341a0ce66ad5fedfba
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-05-01 00:09:36 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
         for (int i = 1; i <= 10; i++) {
             if (n >= factorial(i)) {
                 answer = i;
-            } else {
-                break;
+            }else {
+                break;                
             }
         }
         return answer;
