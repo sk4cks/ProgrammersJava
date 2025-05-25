c700d76b75cc126fb5d21908632be6e49bf44da5
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-05-25 22:57:26 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(int i=0; i<my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if(c > 90) {
+                answer[c % 'a' + 26]++;  
+            }else {
+                answer[c % 'A']++;
+            }
+        }
+        
+        return answer;
+    }
+}
