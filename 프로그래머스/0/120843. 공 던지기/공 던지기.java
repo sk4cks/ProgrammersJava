341345f302f431cbd7cc4a4131d4cd97b77e6c47
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-05-30 22:46:47 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int index = 0;
-        int answer = 0;
         
-        for(int i=0; i<k; i++) {
-            answer = numbers[index % numbers.length];
+        for(int i=1; i<k; i++) {
             index += 2;
         }
         
-        return answer;
+        return numbers[index % numbers.length];
     }
 }
