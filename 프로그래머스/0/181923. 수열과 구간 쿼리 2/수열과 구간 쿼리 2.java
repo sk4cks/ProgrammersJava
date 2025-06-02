2d0db341d646dd89582011405c7d2c464adfc206
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-06-02 23:13:22 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.13 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+        for(int i=0; i<queries.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for(int j=queries[i][0]; j<=queries[i][1]; j++) {
+                if(arr[j] > queries[i][2]) {
+                    min = Math.min(min, arr[j]);
+                }
+            }
+            
+            answer[i] = min == Integer.MAX_VALUE ? -1 : min;
+        }
+        
+        return answer;
+    }
+}
