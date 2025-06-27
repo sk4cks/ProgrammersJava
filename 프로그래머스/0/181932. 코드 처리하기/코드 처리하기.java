92dd3f30f574569672f019800c512efa21ef4285
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-06-27 21:56:23 +0900

    [level 0] Title: 코드 처리하기, Time: 10.18 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder sb = new StringBuilder();
+        boolean mode = false;
+
+        for(int i=0; i<code.length(); i++) {
+            char c = code.charAt(i);
+            if(c == '1') {
+                mode = !mode;
+            }else if((mode && i%2 == 1) || (!mode && i%2 == 0)) {
+                sb.append(c);
+            }
+        }
+        
+        return sb.length() == 0 ? "EMPTY" : sb.toString();
+    }
+}
