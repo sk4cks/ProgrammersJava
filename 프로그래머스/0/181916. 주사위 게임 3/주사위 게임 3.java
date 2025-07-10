b6960db7f1af9279c529647ec62af43745b6bb10
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-07-10 22:59:05 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.62 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,41 +1,27 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int answer = 0;
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = {a,b,c,d};
+        Arrays.sort(arr);
         
-        map.put(a,map.getOrDefault(a,0) + 1);
-        map.put(b,map.getOrDefault(b,0) + 1);
-        map.put(c,map.getOrDefault(c,0) + 1);
-        map.put(d,map.getOrDefault(d,0) + 1);
+        for(int key : arr) {
+            map.put(key, map.getOrDefault(key,0) + 1);
+        }
         
         switch (map.size()) {
             case 1:
                 answer = 1111*a;
                 break;
             case 2:
-                int[] arr = new int[2];
-                int index = 0;
-                for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                    switch (entry.getValue()) {
-                        case 1:
-                            answer += entry.getKey();
-                            break;
-                        case 2:
-                            arr[index++] = entry.getKey();
-                            break;
-                        case 3:
-                            answer += 10 * entry.getKey();
-                            break;
-                    }
-
-                }
-                if(index > 0) {
-                    answer = (arr[0] + arr[1]) * Math.abs(arr[0]-arr[1]);
-                }else{
-                    answer = (int) Math.pow(answer,2);
+                if(arr[1] != arr[2]) {
+                    answer = (arr[1] + arr[2]) * Math.abs(arr[1] - arr[2]);
+                }else {
+                    answer = (int) Math.pow(10 * arr[1] + (arr[0] + arr[3] - arr[1]),2);
                 }
                 break;
             case 3:
@@ -47,10 +33,7 @@ class Solution {
                 }
                 break;
             case 4:
-                answer = Integer.MAX_VALUE;
-                for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                       answer = Math.min(entry.getKey(), answer);
-                }
+                answer = arr[0];
                 break;
         }
         
