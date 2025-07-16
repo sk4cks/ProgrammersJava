3774f265a11925d32643d83134c48dd7923e8bc5
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-07-16 22:17:40 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 1.61 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -3,30 +3,24 @@ class Solution {
         int answer = 0;
         int[][] boxes = new int[(n+w-1)/w][w];
         int startNum = 1;
-        int[] startArr = new int[2];
         int[] targetArr = new int[2];
         
-        Loop:
-        while (startNum <= n) {
-            for(int i=0; i<w; i++) {
-                if(startNum == num) {
-                    targetArr = new int[]{startArr[0],startArr[1]};
+        for(int i=0; i<boxes.length; i++) {
+            if(i%2 == 0) {
+                for(int j=0; j<w &&  startNum <= n ; j++) {
+                    if(startNum == num) {
+                        targetArr = new int[]{i,j};
+                    }
+                    boxes[i][j] = startNum++;
                 }
-                boxes[startArr[0]][startArr[1]++] = startNum++;
-                if(startNum > n) break Loop;
-            }
-            startArr[0]++;
-            startArr[1]--;
-
-            for(int i=w-1; i>=0; i--) {
-                if(startNum == num) {
-                    targetArr = new int[]{startArr[0],startArr[1]};
+            }else{
+                for(int j=w-1; j>=0 && n >= startNum; j--) {
+                    if(startNum == num) {
+                        targetArr = new int[]{i,j};
+                    }
+                    boxes[i][j] = startNum++;
                 }
-                boxes[startArr[0]][startArr[1]--] = startNum++;
-                if(startNum > n) break Loop;
             }
-            startArr[0]++;
-            startArr[1]++;
         }
         
         for(int i=boxes.length-1; i>=targetArr[0]; i--) {
