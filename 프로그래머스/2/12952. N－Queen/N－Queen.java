49138721d7db8ef53731ff4f0ff706980c9583ae
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-07-24 05:41:58 +0900

    [level 2] Title: N-Queen, Time: 162.98 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] board;
+    int answer = 0;
+    public int solution(int n) {
+        board = new int[n];
+        
+        backTracking(0, n);
+        
+        return answer;
+    }
+    
+    void backTracking(int depth, int n) {
+        if (depth == n) {
+            answer++;
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            board[depth] = i; // 해당 depth(행)과 i(열)에 퀸을 놓을 수 있는지 확인
+            if (valid(depth)) {
+                backTracking(depth + 1, n);
+            }
+        }
+    }
+    
+    boolean valid(int i) {
+        for (int j = 0; j < i; j++) { // 마지막으로 놓여진 것과 이전의 것들을 비교
+            if (board[i] == board[j]) return false;
+            if (Math.abs(i - j) == Math.abs(board[i] - board[j])) return false;
+        }
+        return true;
+    }
+}
