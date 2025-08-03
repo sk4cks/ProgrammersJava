52a5cf99054cccf3400de5fdeeafdc8368cbaa6e
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-08-03 17:50:18 +0900

    [level 2] Title: N-Queen, Time: 162.54 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,33 +1,34 @@
 class Solution {
     int[] board;
-    int answer = 0;
+    int answer;
+    
     public int solution(int n) {
-        // 배열의 값은 해당 행의 queen이 있는 '열(column)'을 의미함
         board = new int[n];
-        backTracking(0, n);
+        backTracking(0,n);
         
         return answer;
     }
     
-    // depth는 행을 의미함
-    public void backTracking(int depth, int n) {
-        if (depth == n) {
+    void backTracking(int depth, int n) {
+        if(depth == n) {
             answer++;
             return;
         }
-        for (int i = 0; i < n; i++) {
-            board[depth] = i; // 해당 depth(행)과 i(열)에 퀸을 놓을 수 있는지 확인
-            if (valid(depth)) {
-                backTracking(depth + 1, n);
+
+        for(int i=0; i<n; i++) {
+            board[depth] = i;
+            if(valid(depth)) {
+                backTracking(depth+1, n);
             }
         }
     }
     
-    public boolean valid(int i) {
-        for (int j = 0; j < i; j++) { // 마지막으로 놓여진 것과 이전의 것들을 비교
-            if (board[i] == board[j]) return false;
-            if (Math.abs(i - j) == Math.abs(board[i] - board[j])) return false;
+    boolean valid(int depth) {
+        for(int i=0; i<depth; i++) {
+            if(board[i] == board[depth]) return false;
+            if(Math.abs(i-depth) == Math.abs(board[i] - board[depth])) return false;
         }
+
         return true;
     }
 }
