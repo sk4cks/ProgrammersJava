cb4f5b70f925bba39fed305c1a06e0f9a73ded0c
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-08-22 22:13:21 +0900

    [level 2] Title: 요격 시스템, Time: 387.30 ms, Memory: 197 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -4,15 +4,12 @@ import java.util.Comparator;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        
-        Arrays.sort(targets, Comparator.comparing((int[] o) -> o[1])
-                .thenComparing(o -> o[0]));
+        int end = 0;
 
-        int end = targets[0][1];
-        answer++;
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
 
-        for(int i=1; i<targets.length; i++) {
-            if(end <= targets[i][0]) {
+        for (int i = 0; i < targets.length; i++) {
+            if (end <= targets[i][0]) {
                 end = targets[i][1];
                 answer++;
             }
