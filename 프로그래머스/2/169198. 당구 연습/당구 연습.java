c9d6349ea33dd08f9d8309e907e588743893039f
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-09-22 22:37:17 +0900

    [level 2] Title: 당구 연습, Time: 2.69 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -4,45 +4,42 @@ class Solution {
     public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
         
-        for (int i = 0; i < balls.length; i++) {
-            int targetX = balls[i][0];
-            int targetY = balls[i][1];
-            List<Integer> candidates = new ArrayList<>();
-            
-            // 4가지 반사 좌표
-            int[][] mirrors = {
-                {-targetX, targetY},        // 좌벽 반사
-                {2 * m - targetX, targetY}, // 우벽 반사
-                {targetX, -targetY},        // 아랫벽 반사
-                {targetX, 2 * n - targetY}  // 윗벽 반사
+        for (int i=0; i<balls.length; i++) {
+            List<Integer> distList = new ArrayList<>();
+            int[] target = balls[i];
+            int[][] mirros = {
+                {target[0], 2*n - target[1]}, // 윗벽 반사
+                {2*m - target[0], target[1]}, // 오른벽 반사
+                {target[0], -target[1]}, // 아랫벽 반사
+                {-target[0], target[1]} // 왼벽 반사
             };
-            
-            for (int[] mirror : mirrors) {
-                int mx = mirror[0];
-                int my = mirror[1];
-                
-                int dx = startX - mx;
-                int dy = startY - my;
-                int dist = (int) (Math.pow(dx,2) + Math.pow(dy,2));
-                
-                // 예외 케이스: 같은 x 또는 같은 y 선상에 있을 때
-                if (startX == targetX) {
-                    // 같은 세로선
-                    if ((startY < targetY && targetY < my) || (my < targetY && targetY < startY)) {
+
+            // 목표 공을 좌우/상하 대칭 복제해두고, 내 공에서 목표 공 복제본까지 직선거리 구하기
+            for (int[] mirror : mirros) {
+                int dx = mirror[0] - startX;
+                int dy = mirror[1] - startY;
+                int dist = dx*dx + dy*dy;
+
+                // 같은 X선상 예외처리
+                if (startX == target[0]) {
+                    if ( (startY < target[1] && target[1] < mirror[1]) 
+                        || (mirror[1] < target[1] && target[1] < startY)) {
                         continue;
                     }
                 }
-                if (startY == targetY) {
-                    // 같은 가로선
-                    if ((startX < targetX && targetX < mx) || (mx < targetX && targetX < startX)) {
+
+                // 같은 Y선상 예외처리
+                if (startY == target[1]) {
+                    if ( (startX < target[0] && target[0] < mirror[0]) 
+                        || (mirror[0] < target[0] && target[0] < startX)) {
                         continue;
                     }
                 }
-                
-                candidates.add(dist);
+
+                distList.add(dist);
             }
-            
-            answer[i] = Collections.min(candidates);
+
+            answer[i] = Collections.min(distList);
         }
         
         return answer;
