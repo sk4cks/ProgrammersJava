d078a8b27ad7914f317cfc18c12d2dcc25a4a428
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-10-14 23:15:36 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -6,6 +6,7 @@ class Solution {
 
         for (int i=0; i<length; i++) {
             char letter = name.charAt(i);
+            // 1. 위 아래 이동 카운트
             // 위 또는 아래로 움직이는것중에 최소값 더하기
             answer += Math.min(letter - 'A', 'Z' - letter + 1);
 
@@ -15,6 +16,7 @@ class Solution {
                 next++;
             }
 
+            // 2. 왼쪽 오른쪽 이동 카운트
             max = Math.min(max, i*2 + length-next); // 앞으로 갔다가 되돌아 오는 경우
             max = Math.min(max, (length-next) * 2 + i); // 뒤로 갔다가 되돌아 오는 경우
         }
