40a5d66a4677d2c4db3b3267d3dcb4138e1a6d20
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-10-14 23:13:27 +0900

    [level 2] Title: 조이스틱, Time: 0.09 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,21 +2,24 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         int length = name.length();
-        int max = length-1;
+        int max = length-1; //오른쪽으로만 가는 경우로 기본 max값 설정
 
         for (int i=0; i<length; i++) {
             char letter = name.charAt(i);
+            // 위 또는 아래로 움직이는것중에 최소값 더하기
             answer += Math.min(letter - 'A', 'Z' - letter + 1);
 
+            // 다음문자가 A면 패스하기 위해서 next++해줌
             int next = i+1;
             while (next < name.length() && name.charAt(next) == 'A') {
                 next++;
             }
 
-            max = Math.min(max, i*2 + length-next);
-            max = Math.min(max, (length-next) * 2 + i);
+            max = Math.min(max, i*2 + length-next); // 앞으로 갔다가 되돌아 오는 경우
+            max = Math.min(max, (length-next) * 2 + i); // 뒤로 갔다가 되돌아 오는 경우
         }
 
+        // 위아래 이동 + 왼쪽 오른쪽 이동
         answer += max;
         
         return answer;
