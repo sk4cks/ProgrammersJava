b2d420a89830c48260542c52bb7af65cff8ee3ad
나카롱 <109573477+sk4cks@users.noreply.github.com>
2025-10-20 21:30:48 +0900

    [level 2] Title: 양궁대회, Time: 0.68 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 class Solution {
-    int maxDiff = 0;
-    int[] answer = {-1};
+    int maxDiff = 0;        // 최대 점수 차이
+    int[] answer = {-1};    // 라이언이 못이기는 경우 리턴 기본값
     
     public int[] solution(int n, int[] info) {
         dfs(0, n, info, new int[11]);
@@ -8,51 +10,62 @@ class Solution {
         return answer;
     }
     
-    void dfs(int idx, int arrowsLeft, int[] apeach, int[] ryan) {
-        // 모든 점수 구간 탐색 완료
-        if (idx == 11 || arrowsLeft == 0) {
-            ryan[10] += arrowsLeft; // 남은 화살은 0점 구간에 몰아줌
-            checkScore(apeach, ryan);
-            ryan[10] -= arrowsLeft;
+    // dfs(인덱스, 남은화살, 어피치배열, 라이언배열)
+    void dfs(int idx, int cnt, int[] apeachArr, int[] ryanArr) {
+        
+        // 마지막일경우 0점에 남은화살을 넣고 점수체크
+        if (idx == 11 || cnt == 0) {
+            ryanArr[10] += cnt;
+            checkScore(apeachArr, ryanArr);
+            ryanArr[10] -= cnt;     // 백트래킹을 위한 원복
             return;
         }
 
-        // 1️⃣ 이 점수를 따기 위해 어피치보다 1발 더 쏘는 경우
-        if (arrowsLeft > apeach[idx]) {
-            ryan[idx] = apeach[idx] + 1;
-            dfs(idx + 1, arrowsLeft - ryan[idx], apeach, ryan);
-            ryan[idx] = 0;
+        // 라이언이 점수를 가져올때
+        if (cnt > apeachArr[idx]) {
+            ryanArr[idx] = apeachArr[idx] + 1;
+            dfs(idx + 1, cnt - (apeachArr[idx] + 1), apeachArr, ryanArr);
+            ryanArr[idx] = 0;   // 백트래킹을 위한 원복
         }
 
-        // 2️⃣ 이 점수를 포기하는 경우
-        dfs(idx + 1, arrowsLeft, apeach, ryan);
+        // 라이언이 점수를 포기할때
+        dfs(idx + 1, cnt, apeachArr, ryanArr);
     }
     
-    void checkScore(int[] apeach, int[] ryan) {
-        int aScore = 0, rScore = 0;
+    // 점수체크
+    void checkScore(int[] apeachArr, int[] ryanArr) {
+        int apeachScore = 0;
+        int ryanScore = 0;
+        int diff = 0;
 
-        for (int i = 0; i < 11; i++) {
-            if (apeach[i] == 0 && ryan[i] == 0) continue;
-            if (apeach[i] >= ryan[i]) aScore += 10 - i;
-            else rScore += 10 - i;
+        for (int i=0; i<apeachArr.length; i++) {
+            if (apeachArr[i] > 0 || ryanArr[i] > 0) {   // 둘다 0발이 아닌경우에만
+                if (ryanArr[i] > apeachArr[i]) {
+                    ryanScore += 10 - i;
+                } else {
+                    apeachScore += 10 - i;
+                }
+            }
         }
 
-        int diff = rScore - aScore;
+        diff = ryanScore - apeachScore;
         if (diff <= 0) return;
 
-        // 최대 점수 차 갱신
-        if (diff > maxDiff || (diff == maxDiff && isBetter(ryan, answer))) {
+        // 점수차이가 크거나 || (점수차이가 같은데 낮은 점수에 더 많이 쐈을때)
+        if (diff > maxDiff || (diff == maxDiff && checkLowerScore(answer, ryanArr))) {
             maxDiff = diff;
-            answer = ryan.clone();
+            answer = ryanArr.clone();
         }
     }
-    
-    // 낮은 점수에 더 많은 화살을 쏜 배열 선택
-    boolean isBetter(int[] newR, int[] oldR) {
-        for (int i = 10; i >= 0; i--) {
-            if (newR[i] > oldR[i]) return true;
-            else if (newR[i] < oldR[i]) return false;
+
+    // 가장 낮은 점수를 더 맞힌경우 체크
+    boolean checkLowerScore(int[] oldArr, int[] newArr) {
+        for (int i=oldArr.length-1; i>=0; i--) {
+            if (oldArr[i] < newArr[i]) return true;
+            else if (oldArr[i] > newArr[i]) return false;
         }
+
         return false;
     }
+    
 }
