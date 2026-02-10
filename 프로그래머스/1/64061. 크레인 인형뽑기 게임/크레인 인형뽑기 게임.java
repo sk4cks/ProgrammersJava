4f213986c80575ecdf4e532c8d6461824cdff20d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-02-10 23:07:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.42 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,26 +3,44 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        
+        // 바구니 역할을 하는 스택 (뽑은 인형을 담음)
         Stack<Integer> stack = new Stack<>();
 
-
-        for(int i=0; i<moves.length; i++){
-            int col = moves[i]-1;
-            for(int j=0; j<board.length; j++){
+        // moves 순서대로 크레인을 작동
+        for(int i = 0; i < moves.length; i++){
+            
+            // 선택한 열 (1번부터 시작하므로 -1)
+            int col = moves[i] - 1;
+            
+            // 해당 열의 위에서부터 인형 탐색
+            for(int j = 0; j < board.length; j++){
+                
+                // 인형이 있는 경우 (0이 아님)
                 if(board[j][col] != 0){
+                    
+                    // 인형을 스택에 넣고, 보드에서는 제거
                     stack.push(board[j][col]);
                     board[j][col] = 0;
 
-                    while (stack.size()>=2 &&
+                    // 스택의 맨 위 두 개가 같은 인형이면 제거
+                    while (stack.size() >= 2 &&
                            stack.get(stack.size()-1) == stack.get(stack.size()-2)){
+                        
                         stack.pop();
                         stack.pop();
-                        answer+=2;
+                        
+                        // 제거된 인형 개수 누적
+                        answer += 2;
                     }
+                    
+                    // 인형을 하나 집었으므로 다음 move로 이동
                     break;
                 }
             }
         }
+        
+        // 최종 제거된 인형 수 반환
         return answer;
     }
 }
