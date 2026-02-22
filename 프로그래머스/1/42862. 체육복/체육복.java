60e587ed132eae571326dc6d6c233200cce1ce90
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-02-22 22:26:51 +0900

    [level 1] Title: 체육복, Time: 0.51 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,32 +2,61 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n- lost.length;
         
+        // 처음에는 전체 학생 수에서 도난당한 학생 수를 뺀 값이
+        // 체육복을 가진 학생 수
+        int answer = n - lost.length;
+        
+        // 탐색을 쉽게 하기 위해 정렬
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
+        /*
+         1단계
+         여벌 체육복이 있지만 동시에 도난당한 학생 처리
+         → 자기 체육복을 사용하면 되므로 정상 상태가 됨
+        */
         for(int i=0; i<lost.length; i++){
             for(int j=0; j< reserve.length; j++){
-                if( reserve[j] == lost[i] ){
+                if(reserve[j] == lost[i]){
+                    
+                    // 이미 처리된 학생 표시
                     reserve[j] = -1;
                     lost[i] = -1;
+                    
+                    // 체육복을 입을 수 있는 학생 증가
                     answer++;
                     break;
                 }
             }
         }
         
+        /*
+         2단계
+         아직 체육복이 없는 학생에게
+         앞번호 또는 뒷번호 학생이 빌려줌
+        */
         for(int i=0; i<lost.length; i++){
-            if(lost[i]==-1) continue;
+            
+            // 이미 해결된 학생은 건너뜀
+            if(lost[i] == -1) continue;
+            
             for(int j=0; j< reserve.length; j++){
-                if( reserve[j] == lost[i]-1 || lost[i]+1 == reserve[j] ){
+                
+                // 번호 차이가 1이면 빌려줄 수 있음
+                if(reserve[j] == lost[i] - 1 || 
+                   reserve[j] == lost[i] + 1){
+                    
+                    // 여벌 사용 처리
                     reserve[j] = -1;
+                    
+                    // 체육복 입을 수 있는 학생 증가
                     answer++;
                     break;
                 }
             }
         }
+        
         return answer;
     }
 }
