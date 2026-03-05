0f2e9a2932a424dc9e93be320326864f40e01f4c
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-05 21:41:29 +0900

    [level 1] Title: 소수 찾기, Time: 323.89 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,18 +1,40 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 0;  // 소수의 개수를 저장할 변수
         
-        for(int i=2; i<=n; i++) {
-            if(isPrime(i)) answer++;
+        /*
+         2부터 n까지 모든 숫자를 검사
+         1은 소수가 아니므로 2부터 시작
+        */
+        for(int i = 2; i <= n; i++) {
+            
+            // i가 소수이면 개수 증가
+            if(isPrime(i)) 
+                answer++;
         }
         
         return answer;
     }
     
+    /*
+     주어진 숫자가 소수인지 판별하는 함수
+    */
     boolean isPrime(int n) {
-        for(int i=2; i<=Math.sqrt(n); i++) {
-            if(n%i==0) return false;
+        
+        /*
+         2부터 √n까지만 검사
+         이유:
+         약수는 항상 √n을 기준으로 쌍으로 존재하기 때문
+         → √n보다 큰 약수를 확인할 필요 없음
+        */
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            
+            // 나누어 떨어지면 소수가 아님
+            if(n % i == 0) 
+                return false;
         }
+        
+        // 끝까지 나누어 떨어지지 않으면 소수
         return true;
     }
 }
