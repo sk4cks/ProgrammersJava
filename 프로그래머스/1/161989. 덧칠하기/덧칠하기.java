c5465afac6635cc8675e0df5b8cf52c3c9e51114
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-07 18:51:36 +0900

    [level 1] Title: 덧칠하기, Time: 1.90 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,30 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        int tmp = 0;
-        for(int i=0; i<section.length; i++){
-            if(tmp > section[i]) continue;
+        
+        int answer = 0; // 페인트를 칠한 횟수
+        int tmp = 0;    // 현재 롤러로 칠해진 마지막 위치
+        
+        // 칠해야 할 구역을 순서대로 확인
+        for(int i = 0; i < section.length; i++){
+            
+            // 이미 이전 롤러로 칠해진 구역이라면 건너뜀
+            if(tmp > section[i]) 
+                continue;
+            
             else{
-                tmp = section[i]+m;
+                /*
+                 새로운 롤러 사용
+                 
+                 section[i] 위치부터 시작해서
+                 길이 m 만큼 칠해짐
+                */
+                tmp = section[i] + m;
+                
+                // 롤러 사용 횟수 증가
                 answer++;
             }
         }
+        
         return answer;
     }
 }
