5eabc640a22ab8a10bff006ba83c0c1ba01d8963
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-10 21:44:02 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		int a, b = 0;
+	
+        a = scanner.nextInt();
+
+        b = scanner.nextInt();
+
+		
+		System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+	}
+
+}
