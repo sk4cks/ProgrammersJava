538c3bd7398ce4f89e41adada8f54fcbfd955876
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-12 20:59:08 +0900

    [level 1] Title: 폰켓몬, Time: 3.91 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,23 @@
-import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int length = nums.length;
         
-        nums = Arrays.stream(nums).distinct().toArray();
-        if(length/2 < nums.length){
-            answer = length/2;
-        }else{
-            answer = nums.length;
+        // 포켓몬 종류를 저장할 Set (중복 자동 제거)
+        HashSet<Integer> set = new HashSet<>();
+        
+        // 모든 포켓몬을 Set에 추가 → 중복 제거
+        for(int num : nums){
+            set.add(num);
         }
         
+        // 선택 가능한 포켓몬 수 (전체의 절반)
+        int maxPick = nums.length / 2;
         
-        return answer;
+        // 포켓몬 종류 수
+        int typeCount = set.size();
+        
+        // 둘 중 작은 값이 최대 선택 가능한 서로 다른 종류 수
+        return Math.min(maxPick, typeCount);
     }
 }
