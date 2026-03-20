04143c1444f9cf88c5baa4fdba16ccd475d48654
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-20 19:40:29 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,11 +2,22 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
+        // n이 a보다 크거나 같을 때까지 반복
+        // (빈 병 a개를 모아야 교환 가능)
         while (n >= a){
-            answer += (n/a)*b;
-            n = ((n/a)*b) + (n%a);
+            
+            // 현재 가지고 있는 빈 병 n개로 교환 가능한 콜라 수 계산
+            // (n / a) : 교환 횟수
+            // (n / a) * b : 새로 받는 콜라 병 수
+            answer += (n / a) * b;
+            
+            // 교환 후 남은 병 업데이트
+            // (n / a) * b : 새로 받은 콜라 → 다 마시면 빈 병이 됨
+            // (n % a) : 교환하고 남은 빈 병
+            n = ((n / a) * b) + (n % a);
         }
         
+        // 총 받은 콜라 병 수 반환
         return answer;
     }
 }
