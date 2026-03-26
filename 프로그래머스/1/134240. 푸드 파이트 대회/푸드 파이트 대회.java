b904c8ffac9b8e72ef5f992546fcffa7f55f16ce
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-26 19:43:10 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 24.44 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,30 @@
 class Solution {
     public String solution(int[] food) {
+        
+        // 왼쪽 절반 문자열을 만들 변수
         String answer = "";
+        
+        // food[0]은 물(중앙)이라 제외하고 1번 음식부터 시작
         for(int i=1; i<food.length; i++){
-            int count = food[i]/2;
             
-            if(count>0){
+            // 각 음식은 절반씩 나눠서 양쪽에 배치
+            int count = food[i] / 2;
+            
+            // 배치할 수 있는 개수가 있으면
+            if(count > 0){
                 for(int j=0; j<count; j++){
-                    answer+=i;
+                    
+                    // 왼쪽 절반에 음식 번호 추가
+                    answer += i;
                 }
             }
         }
+        
+        // 오른쪽 절반을 만들기 위해 문자열 뒤집기
         StringBuffer str = new StringBuffer(answer);
         
-        return answer+"0"+str.reverse().toString();
+        // 최종 구조
+        // 왼쪽 + "0"(물) + 오른쪽(왼쪽 뒤집기)
+        return answer + "0" + str.reverse().toString();
     }
 }
