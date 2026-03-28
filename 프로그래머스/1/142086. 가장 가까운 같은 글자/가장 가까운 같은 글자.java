6a16d8281adc2504c95c8ad2e966b70c8f47d2d1
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-03-28 19:16:15 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.73 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,37 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
-        
-        // 각 위치에서 가장 가까운 같은 문자까지의 거리를 저장할 배열
         int[] answer = new int[s.length()];
-        
-        // 문자열을 한 글자씩 분리 (문자 배열처럼 사용)
-        String[] splitString = s.split("");
-        
-        // 첫 번째 문자는 이전에 같은 문자가 없으므로 -1
-        answer[0] = -1;
+        Map<Character, Integer> map = new HashMap<>();
 
-        // 두 번째 문자부터 순회
-        for(int i=1; i< splitString.length; i++){
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
             
-            // 기본값은 -1 (같은 문자가 없을 경우)
-            answer[i] = -1;
-            
-            // 현재 위치 i에서 왼쪽으로 탐색 (가장 가까운 문자 찾기)
-            for(int j= i-1; j>=0; j--){
-                
-                // 같은 문자를 찾으면
-                if(splitString[i].equals(splitString[j])){
-                    
-                    // 현재 위치 - 찾은 위치 = 거리
-                    answer[i] = i - j;
-                    
-                    // 가장 가까운 하나만 찾으면 되므로 종료
-                    break;
-                }
+            if(map.containsKey(c)){
+                answer[i] = i - map.get(c);
+            } else {
+                answer[i] = -1;
             }
+            
+            map.put(c, i); // 현재 위치 저장
         }
         
-        // 결과 반환
         return answer;
     }
 }
