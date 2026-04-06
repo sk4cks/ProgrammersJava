c2c59e26d19c0ec7091fdf7008e01597add2c4ed
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-04-06 21:42:41 +0900

    [level 1] Title: 직사각형 별찍기, Time: 253.89 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 class Solution {
     public static void main(String[] args) {
+        
+        // 사용자 입력을 받기 위한 Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
+        
+        // 가로 길이 (별의 개수)
         int a = sc.nextInt();
+        
+        // 세로 길이 (줄의 개수)
         int b = sc.nextInt();
         
+        // 세로(b)만큼 반복 → 줄 개수
         for(int i=0; i<b; i++) {
             
+            // 가로(a)만큼 반복 → 한 줄에 별 출력
             for(int j=0; j<a; j++) 
                 System.out.print("*");
             
+            // 한 줄 출력 후 줄바꿈
             System.out.println();
         }
 
