96125d4140166e1146c6e3cc77311109af76b755
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-04-09 20:13:06 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int[] a, int[] b) {
+        
+        // 결과값 (두 벡터의 내적)
         int answer = 0;
         
+        // 배열을 순회하면서 같은 인덱스끼리 곱함
         for(int i=0; i<a.length; i++) {
-            answer += a[i]*b[i];
+            
+            // a[i] * b[i] 값을 누적
+            answer += a[i] * b[i];
         }
         
+        // 최종 내적 결과 반환
         return answer;
     }
 }
