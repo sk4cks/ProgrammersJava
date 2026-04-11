abe0fcb3c3a3794bed0b59ccf8f32d3c7408e53d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-04-11 18:28:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,11 +1,19 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
+        
+        // 최종 합을 저장할 변수
         int answer = 0;
 
+        // 배열을 순회하면서 값과 부호를 함께 처리
         for(int i=0; i<absolutes.length; i++){
+            
+            // signs[i]가 true면 양수, false면 음수
+            // true  → +absolutes[i]
+            // false → -absolutes[i]
             answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
         
+        // 최종 결과 반환
         return answer;
     }
 }
