3bbbf8e1ac9df429a578a6952c00d66c3d14b325
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-04-18 23:00:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 15.87 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -7,7 +7,7 @@ class Solution {
         // 각 라운드(총 3번)의 점수를 저장할 배열
         int[] arr = new int[3];
         
-        // 숫자를 임시로 저장할 문자열 (10 처리 때문에 필요)
+        // 숫자를 임시로 저장할 문자열 (10 같은 두 자리 숫자 처리용)
         String temp = "";
         
         // 현재 몇 번째 라운드인지 가리키는 인덱스
@@ -17,47 +17,47 @@ class Solution {
         for(int i=0; i<dartResult.length(); i++){
             char word = dartResult.charAt(i);
 
-            // 옵션: 아차상 (#) → 해당 점수 음수 처리
+            // [옵션] 아차상 (#) → 해당 라운드 점수를 음수로 변경
             if(word == '#') {
                 arr[idx-1] *= -1;
             }
             
-            // 옵션: 스타상 (*) 
-            // 현재 점수와 바로 이전 점수를 2배로 만듦
+            // [옵션] 스타상 (*) 
+            // 현재 점수와 이전 점수를 각각 2배로 만듦
             else if(word == '*') {
                 for(int j=idx-1; j>=idx-2; j--) {
                     arr[j] *= 2;
                     
-                    // 배열 범위를 벗어나지 않도록
+                    // 인덱스가 0보다 작아지지 않도록 방지
                     if(j == 0) break;
                 }
             }
             
-            // 숫자인 경우 (0~9)
-            // 숫자를 이어붙여서 저장 (10 처리)
+            // [숫자] (0~9)
+            // 숫자를 이어붙여서 저장 (예: '1' 다음 '0' → "10")
             else if(48 <= word && word <= 57) {
                 temp += dartResult.charAt(i);
             }
             
-            // 보너스 영역 (S, D, T)
+            // [보너스] S, D, T 처리
             else {
                 int pow = 1;
                 
-                // S = 1제곱
-                if(word == 'D') pow = 2;
                 // D = 2제곱
-                else if(word == 'T') pow = 3;
+                if(word == 'D') pow = 2;
                 // T = 3제곱
+                else if(word == 'T') pow = 3;
+                // S는 기본값 1제곱
 
-                // temp에 저장된 숫자를 제곱 적용 후 저장
+                // temp에 저장된 숫자를 제곱하여 현재 라운드 점수로 저장
                 arr[idx++] = (int) Math.pow(Integer.parseInt(temp), pow);
                 
-                // 다음 숫자를 위해 초기화
+                // 다음 숫자를 위해 temp 초기화
                 temp = "";
             }
         }
 
-        // 3라운드 점수 합산
+        // 모든 라운드 점수 합산
         answer = Arrays.stream(arr).sum();
         return answer;
     }
