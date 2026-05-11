bbcc75dcbf9ba336d7a360d500b5c60fbb4329cb
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-05-11 21:08:16 +0900

    [level 1] Title: 노란불 신호등, Time: 6.86 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,76 +1,112 @@
 class Solution {
-    // 신호등 정보를 저장할 부분
-    class TrafficLight {
-        int greenLimit;
-        int yelloLimit;
-        int totalTime;
 
-        public TrafficLight(int green, int yello, int red) {
-            this.greenLimit = green;
-            this.yelloLimit = green + yello;
-            this.totalTime = green + yello + red;
+    // 신호등 정보 클래스
+    class TrafficLight {
+
+        int greenEnd;   // 초록불 끝나는 시간
+        int yellowEnd;  // 노란불 끝나는 시간
+        int cycle;      // 전체 주기
+
+        public TrafficLight(int green, int yellow, int red) {
+            this.greenEnd = green;
+            this.yellowEnd = green + yellow;
+            this.cycle = green + yellow + red;
         }
-        // 노란불인지 확인
-        public boolean isYello(int time) {
-            // 시간 보정
-            int colorTime = time % totalTime == 0 ? totalTime : time % totalTime;
-            if (greenLimit < colorTime && colorTime <= yelloLimit) {
-                return true;
+
+        // 특정 시간에 노란불인지 확인
+        boolean isYellow(int time) {
+
+            /*
+             현재 사이클 기준 시간 계산
+             예:
+             cycle = 10
+             time = 13 → current = 3
+            */
+            int current = time % cycle;
+
+            // 나머지가 0이면 사이클 마지막 시간
+            if (current == 0) {
+                current = cycle;
             }
-            return false;
+
+            // 노란불 범위인지 확인
+            return greenEnd < current && current <= yellowEnd;
         }
     }
 
     public int solution(int[][] signals) {
-        // 만든 클래스에 적용
-        TrafficLight[] trafficLights = new TrafficLight[signals.length];
-        // 최대 검사 시간을 구하기 위한 배열
-        int[] totalTimes = new int[signals.length];
-        for (int i = 0; i < signals.length; i++) {
-            trafficLights[i] =
-                    new TrafficLight(signals[i][0], signals[i][1], signals[i][2]);
-            totalTimes[i] = trafficLights[i].totalTime;
+
+        int n = signals.length;
+
+        TrafficLight[] lights = new TrafficLight[n];
+        int[] cycles = new int[n];
+
+        // 신호등 객체 생성
+        for (int i = 0; i < n; i++) {
+            lights[i] = new TrafficLight(
+                    signals[i][0],
+                    signals[i][1],
+                    signals[i][2]
+            );
+
+            cycles[i] = lights[i].cycle;
         }
-        // 모두 노란불이 가능한지 판단하기 위한 최소 시간
-        int maxTime = getLcm(totalTimes);
-        for (int time = 1; time <= maxTime; time++) {
-            boolean isEqaul = true;
-            // 신호등 모드 노란불인지 확인
-            for (int i = 0; i < trafficLights.length; i++) {
-                if (!trafficLights[i].isYello(time)) {
-                    isEqaul = false;
+
+        /*
+         모든 신호 패턴은 최소공배수 이후 반복됨
+         → LCM까지만 검사하면 충분
+        */
+        int limit = getLcm(cycles);
+
+        // 모든 시간 탐색
+        for (int time = 1; time <= limit; time++) {
+
+            boolean isAllYellow = true;
+
+            // 모든 신호등이 노란불인지 확인
+            for (TrafficLight light : lights) {
+                if (!light.isYellow(time)) {
+                    isAllYellow = false;
                     break;
                 }
             }
-            // 노란불인 경우 해당 시간 반환
-            if (isEqaul) {
+
+            // 전부 노란불이면 해당 시간 반환
+            if (isAllYellow) {
                 return time;
             }
         }
-        // 불가능하면 -1
+
+        // 끝까지 없으면 불가능
         return -1;
     }
 
-    public int getLcm(int[] nums) {
-        // 1인 경우는 자기 자신이 최소 공배수
-        if (nums.length == 1) {
-            return nums[0];
+    // 배열 전체의 최소공배수 계산
+    int getLcm(int[] nums) {
+
+        int lcm = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            lcm = lcm(lcm, nums[i]);
         }
-        // 첫 2개의 최대 공약수 구하여 최소 공배수 구하기
-        int gcd = getGcd(nums[0], nums[1]);
-        int lcm = (nums[0] * nums[1]) / gcd;
-        // 2개 이상인 경우 계속 최소 공배수 구하기
-        for (int i = 2; i < nums.length; i++) {
-            gcd = getGcd(nums[i], lcm);
-            lcm = (lcm * nums[i]) / gcd;
-        }
+
         return lcm;
     }
-    // 최대 공약수 구하기
-    private int getGcd(int num1, int num2) {
-        if (num1 % num2 == 0) {
-            return num2;
+
+    // 두 수의 최소공배수
+    int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    // 최대공약수 (유클리드 호제법)
+    int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-        return getGcd(num2, num1 % num2);
+
+        return a;
     }
 }
