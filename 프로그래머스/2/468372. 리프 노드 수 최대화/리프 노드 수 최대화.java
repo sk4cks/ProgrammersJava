88060e5bcb858b349e12d74dcaf4ab77b4e49f7d
나카롱 <109573477+sk4cks@users.noreply.github.com>
2026-05-29 14:33:22 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 48.19 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -7,22 +7,28 @@ class Solution {
         return answer;
     }
 	
-	void dfs(long active, long used, long split, long leaf, int dist_limit, int split_limit) {
-        if (used > dist_limit) return;
+	//active: 분배가능한 활성화 상태의 노드 수
+    //used: 사용한 노드 수
+    //split: 현재까지 분배도
+    //leaf: 분배 불가한 확정된 leaf노드의 수
+    void dfs(long active, long used, long split, long leaf, int dist_limit, int split_limit) {
+        if (used > dist_limit) {
+            return;
+        }
+        
+        // (현재 활성화 상태 노드 수 + 확정된 leaf 노드 수) 또는 answer 중 최대값
+        answer = (int) Math.max(active + leaf, answer);
 
-        answer = (int) Math.max(leaf + active, answer);
-
+        // 자식개수는 2 또는 3
         for (int child = 2; child <= 3; child++) {
-            long nextSplit = split * child;
+            long nextSplit = child * split;
 
             if (nextSplit <= split_limit) {
-                long nextNode = active * child;
+                long nextNode = active * child; // 분배 가능한 노드 수 * 자식 수
+                long remain = dist_limit - used; // 사용가능한 남은 노드 수
 
-                long remain = dist_limit - used;
-
-                long nextActive = Math.min(nextNode, remain);
-
-                long nextLeaf = leaf + nextNode - nextActive;
+                long nextActive = Math.min(nextNode, remain); // 다음번 분배가능한 노드 수
+                long nextLeaf = leaf + nextNode - nextActive; // leaf + 탈락된 노드 수
 
                 dfs(nextActive, used + nextActive, nextSplit, nextLeaf, dist_limit, split_limit);
             }
